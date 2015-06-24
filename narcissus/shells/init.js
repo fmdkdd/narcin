426882e52607930273a3eae014ebd94cd33fdd15
fmdkdd <fmdkdd@gmail.com>
2015-06-24 17:41:14 +0200

    Forget jsdecomp (for --parse-only option of njs)

--- narcissus/shells/init.js
+++ narcissus/shells/init.js
@@ -2,3 +2,4 @@ loadRelativeToScript('../lib/jsdefs.js');
 loadRelativeToScript('../lib/jslex.js');
 loadRelativeToScript('../lib/jsparse.js');
 loadRelativeToScript('../lib/jsresolve.js');
+loadRelativeToScript('../lib/jsdecomp.js');
